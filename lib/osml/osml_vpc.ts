3ece97aca1f687e21422472dfe12ad3f824bc36d
drduhe <drduhe@amazon.com>
2023-09-20 10:49:41 -0600

    Enabling VPC endpoints

--- lib/osml/osml_vpc.ts
+++ lib/osml/osml_vpc.ts
@@ -1,10 +1,22 @@
 /*
  * Copyright 2023 Amazon.com, Inc. or its affiliates.
  */
-import { IVpc, SelectedSubnets, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+import {
+  GatewayVpcEndpointAwsService,
+  InterfaceVpcEndpointAwsService,
+  InterfaceVpcEndpointService,
+  IVpc,
+  SelectedSubnets,
+  SubnetType,
+  Vpc
+} from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 
+import { OSMLAccount } from "./osml_account";
+
 export interface OSMLVpcProps {
+  // the osml deployment account
+  account: OSMLAccount;
   // name of the VPC to create
   vpcName?: string;
   // the vpc id to import
@@ -25,7 +37,7 @@ export class OSMLVpc extends Construct {
    */
   constructor(scope: Construct, id: string, props: OSMLVpcProps) {
     super(scope, id);
-    // if a osmlVpc id is not explicitly given use the default osmlVpc
+    // if an osmlVpc id is not explicitly given, use the default osmlVpc
     if (props.vpcId) {
       this.vpc = Vpc.fromLookup(this, "OSMLImportVPC", {
         vpcId: props.vpcId,
@@ -52,6 +64,49 @@ export class OSMLVpc extends Construct {
       this.selectedSubnets = vpc.selectSubnets({
         subnetType: SubnetType.PRIVATE_WITH_EGRESS
       });
+
+      // create custom endpoint prefix's for know ADC regions requiring it
+      let partitionPrefix;
+      if (props.account.region === "us-iso-east-1") {
+        partitionPrefix = "gov.ic.c2s";
+      } else if (props.account.region === "us-isob-east-1") {
+        partitionPrefix = "gov.sgov.sc2s";
+      }
+
+      // create vpc endpoints
+      this.vpc.addGatewayEndpoint("S3GatewayEndpoint", {
+        service: GatewayVpcEndpointAwsService.S3
+      });
+      this.vpc.addInterfaceEndpoint("SMApiInterfaceEndpoint", {
+        service: partitionPrefix
+          ? new InterfaceVpcEndpointService(
+              `${partitionPrefix}.${props.account.region}.sagemaker.api`
+            )
+          : InterfaceVpcEndpointAwsService.SAGEMAKER_API,
+        privateDnsEnabled: true
+      });
+      this.vpc.addInterfaceEndpoint("SMRuntimeInterfaceEndpoint", {
+        service: partitionPrefix
+          ? new InterfaceVpcEndpointService(
+              `${partitionPrefix}.${props.account.region}.sagemaker.runtime`
+            )
+          : InterfaceVpcEndpointAwsService.SAGEMAKER_RUNTIME,
+        privateDnsEnabled: true
+      });
+      // certain endpoints are not supported in ADC regions
+      if (!props.account.isAdc) {
+        this.vpc.addGatewayEndpoint("DDBGatewayEndpoint", {
+          service: GatewayVpcEndpointAwsService.DYNAMODB
+        });
+        this.vpc.addInterfaceEndpoint("CWInterfaceEndpoint", {
+          service: InterfaceVpcEndpointAwsService.CLOUDWATCH,
+          privateDnsEnabled: true
+        });
+        this.vpc.addInterfaceEndpoint("CWLogsInterfaceEndpoint", {
+          service: InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+          privateDnsEnabled: true
+        });
+      }
     }
   }
 }
